d590a373141e8ac7baf55bce4598ad63e72c8024
Nyx <61583597+mrlittleman@users.noreply.github.com>
2023-02-13 22:25:54 +0800

    <3

--- CropPrediction/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.HomeView, name="home"),
-    path('home/', views.HomeView, name="home"),
-    path('uploads-files/', views.UploadFilesView, name="upload-files"),
-   
-]
